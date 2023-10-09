ddc2810c848a8a21f20e75f9d26a8176ed7684ff
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2023-10-09 15:05:08 +0000

    remove server, add firebase BaaS

--- divvy/App.tsx
+++ divvy/App.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="italic text-white">Open up App.tsx to start working on your app!</Text>
+      <Text className="text-white">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
