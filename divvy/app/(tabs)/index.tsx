e5c3d288c4ab3b584cdde03abb7a97da3179b9ee
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2023-10-12 13:26:54 +0000

    refactor group logic to use hooks

--- divvy/app/(tabs)/index.tsx
+++ divvy/app/(tabs)/index.tsx
@@ -1,53 +1,44 @@
-import { KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { router } from 'expo-router';
-import { collection,getDocs,query,where } from 'firebase/firestore';
-import { auth,db } from '../../config/firebase';
-import { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+import { collection, query, where } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 export default function TabOneScreen() {
-  const [groups, setGroups] = useState([])
+  const [user] = useAuthState(auth);
 
-  useEffect(() => {
-    getGroups().then((res) => {
-      //@ts-ignore
-      let group = []
-      res.forEach(g => {
-        group.push(g.id)
-      })      
-      //@ts-ignore
-      setGroups(group)
+  const groupRef = collection(db, "groups");
+  const q = query(groupRef, where("users", "array-contains", user?.uid));
+  const [groups, loading, error] = useCollectionData(q);
 
-    })
-
-
-  }, [])
-  
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
-     <View className='flex-1 items-center justify-start'>
-       <View className="w-3/4 mt-10">
+    <View className="flex-1 items-center justify-start">
+      <View className="w-3/4 mt-10">
         <TouchableOpacity
-          onPress={() => router.push('/createGroup')}
+          onPress={() => router.push("/createGroup")}
           className=" w-full p-2 rounded-xl items-center border"
         >
-          <Text className="text-black font-bold text-base"> + Add new party &#129395;</Text>
+          <Text className="text-black font-bold text-base">
+            {" "}
+            + Add new party &#129395;
+          </Text>
         </TouchableOpacity>
       </View>
-      <View className='flex-1 justify-center h-full'>
-        {groups.map(g => <Text>Group {g} </Text>)}
+      <View className="flex-1 justify-center h-full">
+        {groups?.map((group) => (
+          /*REVEIW: Better key here, groupName might not always be unique */
+          <Text key={group.groupName}>Group {group.groupName} </Text>
+        ))}
       </View>
-      
-     </View>
+    </View>
   );
 }
-
-const getGroups = async () =>{
-  const user = auth.currentUser?.uid
-
-  const groupRef = collection(db,'groups')
-  const q = query(groupRef, where('users','array-contains',user))
-  const snapshot = await getDocs(q)
-
-  return snapshot;
-}
-
