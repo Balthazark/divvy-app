08ef7c042c53e987e85e7db81ad0e32ef9641612
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2023-10-22 15:48:29 +0000

    implment ui for modal for adding friends

--- divvy/app/addFriendModal.tsx
+++ divvy/app/addFriendModal.tsx
@@ -6,32 +6,79 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-import { View } from "react-native";
-import { auth, db } from "../config/firebase";
+import { View, Text } from "react-native";
+import { db } from "../config/firebase";
 import { useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
-import FriendList from "../components/FriendList";
+import { Friend, UserMetaData } from "../types/user";
+import { collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { Avatar } from "../components/Item";
 
 export default function addFriendModal() {
-  const [seachParam, setSearchParam] = useState("");
+  const [searchParam, setSearchParam] = useState("");
+  const [value, loading, error] = useCollection(collection(db, "users"));
+
+  const filteredUsers = value?.docs.filter((user) => {
+    const { email } = user.data();
+    return email?.toLowerCase().includes(searchParam.toLowerCase());
+  });
 
   const keyboardVerticalOffset = Platform.OS === "ios" ? 60 : 0;
 
+  const handleFriendRequest = (user: Friend) => {
+    
+  };
+
+
   return (
     <KeyboardAvoidingView
-      className="flex-1 justify-center items-center"
+      className="flex-1 justify-center items-center w-full"
       behavior="padding"
       keyboardVerticalOffset={keyboardVerticalOffset}
     >
       <ScrollView>
-        <View>
-          <TextInput
-            placeholder="Search for user email"
-            value={seachParam}
-            onChange={(event) => setSearchParam(event.nativeEvent.text)}
-            className="bg-white p-2 rounded-xl mt-1"
-          ></TextInput>
-          <FriendList></FriendList>
+        <TextInput
+          placeholder="Search for user email"
+          value={searchParam}
+          onChange={(event) => setSearchParam(event.nativeEvent.text)}
+          className="bg-white p-2 rounded-xl mt-4 mx-auto min-w-[90%]"
+        ></TextInput>
+        <View className="mt-8 bg-white border-b border-slate-300 rounded-xl">
+          {filteredUsers?.length === 0 ? (
+            <Text className="text-lg p-2">No users found</Text>
+          ) : (
+            filteredUsers?.map((user) => {
+              const { email, name, lastName } = user.data() as UserMetaData;
+              return (
+                <View
+                  className="flex-row items-center justify-between p-4 space-x-4"
+                  key={user.id}
+                >
+                  <Avatar userId={user.id} />
+                  <View className="flex-grow">
+                    <Text className="text-md">{email}</Text>
+                  </View>
+
+                  <TouchableOpacity
+                    onPress={() =>
+                      handleFriendRequest({
+                        userId: user.id,
+                        email,
+                        name,
+                        lastName,
+                      })
+                    }
+                    className="bg-white border-[#0782F9] border-2 p-2 rounded-xl items-center"
+                  >
+                    <Text className="text-[#0782F9] font-bold text-base">
+                      Add
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })
+          )}
         </View>
       </ScrollView>
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
